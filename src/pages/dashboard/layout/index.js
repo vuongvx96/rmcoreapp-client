710efdb8ae68a000379f9e50bd55947a8984d9d7
Vuongvx <vuongvx@hotmail.com>
2019-06-14 08:26:46 +0700

    aaa

--- src/pages/dashboard/layout/index.js
+++ src/pages/dashboard/layout/index.js
@@ -37,6 +37,7 @@ class NewLayout extends React.PureComponent {
 	constructor(props) {
 		super(props)
 		jwt.verify(localStorage.getItem('access_token'), PRIVATE_KEY, (error) => {
+			console.log(error)
 			if (!!error) {
 				this.props.onLogout()
 			}
