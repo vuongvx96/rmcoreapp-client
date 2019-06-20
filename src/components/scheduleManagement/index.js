c3a78eb1c7d65176341f4788d0f0d1e7c62d98cc
Vuongvx <vuongvx@hotmail.com>
2019-06-20 22:54:07 +0700

    update schedule by user

--- src/components/scheduleManagement/index.js
+++ src/components/scheduleManagement/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
 import { AgGridReact } from 'ag-grid-react'
-import { DatePicker, Select } from 'antd'
+import { DatePicker, Select, Button, Icon, Tag } from 'antd'
 import moment from 'moment'
 
 import locale from 'antd/lib/date-picker/locale/vi_VN'
+import { dateFormatter } from '../util/formatter'
 import ModalForm from '../template/modalForm'
+import './index.less'
 
 @inject('scheduleStore')
 @observer
@@ -14,28 +16,90 @@ class ScheduleManagement extends React.Component {
     super(props)
     this.state = {
       timeType: 1,
-      isOpen: false,
-      time: moment()
+      isOpen1: false,
+      startValue: moment(),
+      isOpen2: false,
+      endValue: moment()
     }
 
     this.columnDefs = [
-      { headerName: 'Ngày bắt đầu', field: 'startDate', sortable: true },
-      { headerName: 'Thời gian', field: 'dayParting', sortable: true },
-      { headerName: 'Phòng máy', field: 'roomId', sortable: true },
-      { headerName: 'Môn học', field: 'groupPractice.subject.subjectName', sortable: true },
+      {
+        headerName: 'Ngày bắt đầu',
+        field: 'schedule.startDate',
+        width: 120,
+        sortable: true,
+        valueFormatter: (params) => dateFormatter(params.value) },
+      {
+        headerName: 'Thời gian',
+        field: 'dayParting',
+        sortable: true,
+        width: 95,
+        valueFormatter: (params) => params.value === 1 ? 'Sáng' : params.value === 2 ? 'Chiều' : 'Tối'
+      },
+      {
+        headerName: 'Phòng máy',
+        field: 'schedule.roomId',
+        width: 110,
+        sortable: true
+      },
+      {
+        headerName: 'Môn học',
+        field: 'schedule.groupPractice.subject.subjectName',
+        width: 170,
+        tooltipField: 'schedule.groupPractice.subject.subjectName',
+        sortable: true
+      },
       {
         headerName: 'Giảng viên',
-        field: 'groupPractice.teacher.firstName',
+        field: 'schedule.groupPractice.teacher.firstName',
+        width: 130,
+        tooltipField: 'schedule.groupPractice.teacherId',
         sortable: true,
-        valueFormatter: (params) => params.data.groupPractice.teacher.lastName + ' ' + params.value
+        valueFormatter: (params) => params.data.schedule.groupPractice.teacher.lastName + ' ' + params.value
       },
-      { headerName: 'Loại lặp', field: 'repeatType', sortable: true },
-      { headerName: 'Số lần lặp', field: 'count', sortable: true },
-      { headerName: 'Mô tả', field: 'description', tooltipField: 'description', sortable: true },
-      { headerName: 'Đã xác nhận', field: 'confirmed', sortable: true },
-      { headerName: 'Ngày đăng ký', field: 'createdDate', sortable: true },
-      { headerName: 'Ngày cập nhật', field: 'modifiedDate', sortable: true },
-      { headerName: 'User cập nhật', field: 'modifiedBy', sortable: true }
+      { 
+        headerName: 'Loại lặp',
+        field: 'schedule.repeatType',
+        width: 90,
+        sortable: true,
+        valueFormatter: (params) => params.value === 1 ? 'Theo tuần' : params.value === 2 ? 'Theo tháng' : 'Theo ngày'
+      },
+      {
+        headerName: 'Số lần lặp',
+        field: 'count',
+        width: 100,
+        sortable: true
+      },
+      { headerName: 'Chi tiết', field: 'description', tooltipField: 'description', sortable: true },
+      {
+        headerName: 'Xác nhận',
+        field: 'schedule.confirmed',
+        width: 120,
+        sortable: true,
+        cellRendererFramework: (params) => params.value
+          ? <Tag color='#108EE9'>Đã xác nhận</Tag>
+          : <Tag color='#f50'>Chưa xác nhận</Tag>
+      },
+      {
+        headerName: 'Ngày đăng ký',
+        field: 'schedule.createdDate',
+        width: 120,
+        sortable: true,
+        valueFormatter: (params) => dateFormatter(params.value)
+      },
+      {
+        headerName: 'Ngày cập nhật',
+        field: 'schedule.modifiedDate',
+        width: 120,
+        sortable: true,
+        valueFormatter: (params) => dateFormatter(params.value)
+      },
+      {
+        headerName: 'User cập nhật',
+        field: 'updatedBy',
+        width: 130,
+        sortable: true
+      }
     ]
 
     this.gridOptions = {
@@ -55,8 +119,30 @@ class ScheduleManagement extends React.Component {
     this.setState({ timeType: value })
   }
 
+  disabledStartDate = startValue => {
+    const { endValue } = this.state
+    if (!startValue || !endValue) {
+      return false
+    }
+    return startValue.valueOf() > endValue.valueOf()
+  }
+
+  disabledEndDate = endValue => {
+    const { startValue } = this.state
+    if (!endValue || !startValue) {
+      return false
+    }
+    return endValue.valueOf() <= startValue.valueOf()
+  }
+
+  componentDidMount(){
+    const { startValue, endValue } = this.state
+    this.props.scheduleStore.getAllScheduleByUser(startValue, endValue)
+  }
+
   render() {
-    const { timeType, isOpen, time } = this.state
+    const { timeType, isOpen1, startValue, isOpen2, endValue } = this.state
+    const { getScheduleByUsersJS } = this.props.scheduleStore
     return (
       <div className='schedule-management'>
         <ModalForm
@@ -74,30 +160,89 @@ class ScheduleManagement extends React.Component {
                 <Select.Option key={3} value={3}>Theo năm</Select.Option>
               </Select>
                 <DatePicker
+                  disabledDate={this.disabledStartDate}
+                  allowClear={false}
                   placeholder='Chọn ngày'
                   style={{ width: 140 }}
                   locale={locale}
-                  value={time}
-                  open={isOpen}
+                  value={startValue}
+                  open={isOpen1}
                   format={timeType === 1 ? 'DD/MM/YYYY' : timeType === 2 ? 'MM/YYYY' : 'YYYY' }
                   mode={timeType === 1 ? 'date' : timeType === 2 ? 'month' : 'year'}
                   onOpenChange={(status) => {
                     if (status) {
-                      this.setState({ isOpen: true })
+                      this.setState({ isOpen1: true })
                     } else {
-                      this.setState({ isOpen: false })
+                      this.setState({ isOpen1: false })
                     }
                   }}
                   onPanelChange={(v) => {
+                    if (timeType === 2) {
+                      v.set('date', 1)
+                    } else if (timeType === 3) {
+                      v.set('date', 1)
+                      v.set('month', 0)
+                    }
                     this.setState({
-                      time: v,
-                      isOpen: false
+                      startValue: v,
+                      isOpen1: false
                     })
                   }}
                   onChange={(date) => {
-                    this.setState({ time: date })
+                    if (timeType === 2) {
+                      date.set('date', 1)
+                    } else if (timeType === 3) {
+                      date.set('date', 1)
+                      date.set('month', 0)
+                    }
+                    this.setState({ startValue: date })
                   }}
                 />
+                <Icon type='minus' style={{ marginRight: 10 }}/>
+                <DatePicker
+                  disabledDate={this.disabledEndDate}
+                  allowClear={false}
+                  placeholder='Chọn ngày'
+                  style={{ width: 140 }}
+                  locale={locale}
+                  value={endValue}
+                  open={isOpen2}
+                  format={timeType === 1 ? 'DD/MM/YYYY' : timeType === 2 ? 'MM/YYYY' : 'YYYY' }
+                  mode={timeType === 1 ? 'date' : timeType === 2 ? 'month' : 'year'}
+                  onOpenChange={(status) => {
+                    if (status) {
+                      this.setState({ isOpen2: true })
+                    } else {
+                      this.setState({ isOpen2: false })
+                    }
+                  }}
+                  onPanelChange={(v) => {
+                    if (timeType === 2) {
+                      v.set('date', 1)
+                    } else if (timeType === 3) {
+                      v.set('date', 1)
+                      v.set('month', 0)
+                    }
+                    this.setState({
+                      endValue: v,
+                      isOpen2: false
+                    })
+                  }}
+                  onChange={(date) => {
+                    if (timeType === 2) {
+                      date.set('date', 1)
+                    } else if (timeType === 3) {
+                      date.set('date', 1)
+                      date.set('month', 0)
+                    }
+                    this.setState({ endValue: date })
+                  }}
+                />
+                <Button disabled={startValue > endValue} onClick={() => {
+                  this.props.scheduleStore.getAllScheduleByUser(startValue, endValue)
+                }}>
+                  Xem
+                </Button>
             </>
           }
         >
@@ -105,7 +250,7 @@ class ScheduleManagement extends React.Component {
         <div style={{ height: 'calc(100vh - 132px)' }} className='ag-theme-balham'>
           <AgGridReact
             columnDefs={this.columnDefs}
-            rowData={[]}
+            rowData={getScheduleByUsersJS}
             gridOptions={this.gridOptions}
             onGridReady={this.onGridReady}
           />
--- src/tools/mobx/schedule/index.js
+++ src/tools/mobx/schedule/index.js
@@ -3,149 +3,173 @@ import moment from 'moment'
 import http from '../../axios'
 
 class ScheduleStore {
-    @observable scheduleByWeek = {
-        morning: {
-            0: null,
-            1: null,
-            2: null,
-            3: null,
-            4: null,
-            5: null,
-            6: null
-        },
-        afternoon: {
-            0: null,
-            1: null,
-            2: null,
-            3: null,
-            4: null,
-            5: null,
-            6: null
-        },
-        evening: {
-            0: null,
-            1: null,
-            2: null,
-            3: null,
-            4: null,
-            5: null,
-            6: null
+  @observable scheduleByWeek = {
+    morning: {
+      0: null,
+      1: null,
+      2: null,
+      3: null,
+      4: null,
+      5: null,
+      6: null
+    },
+    afternoon: {
+      0: null,
+      1: null,
+      2: null,
+      3: null,
+      4: null,
+      5: null,
+      6: null
+    },
+    evening: {
+      0: null,
+      1: null,
+      2: null,
+      3: null,
+      4: null,
+      5: null,
+      6: null
+    }
+  }
+
+  @observable scheduleWithDetails = []
+  @observable scheduleByUsers = []
+
+  @observable loading = false
+  @observable year = new Date().getFullYear()
+  @observable week = new Date().getWeek()
+  @observable roomId = 'G8-103'
+  @observable days = new Array(7)
+  @observable currentWeek = new Date().getWeek()
+  @observable keyword = null
+
+  @action getScheduleByWeek = async (fromDate, toDate, roomId) => {
+    this.loading = true
+    try {
+      let params = {
+        fromDate: fromDate,
+        toDate: toDate,
+        roomId: roomId
+      }
+      const response = await http.get('/schedule/week', { params })
+      runInAction('fetch schedule by week', () => {
+        if (response.status === 200) {
+          this.scheduleByWeek = response.data
+          this.year = response.data.year
+          this.week = response.data.week
+          this.roomId = response.data.roomId
+          let day = moment(response.data.fromDate, 'DD/MM/YYYY')
+          this.days = createDaysOfWeek(new Date(day))
         }
+        this.loading = false
+      })
+    } catch (err) {
+      this.loading = false
     }
+  }
 
-    @observable scheduleWithDetails = []
-
-    @observable loading = false
-    @observable year = new Date().getFullYear()
-    @observable week = new Date().getWeek()
-    @observable roomId = 'G8-103'
-    @observable days = new Array(7)
-    @observable currentWeek = new Date().getWeek()
-    @observable keyword = null
-
-    @action getScheduleByWeek = async (fromDate, toDate, roomId) => {
-        this.loading = true
-        try {
-            let params = {
-                fromDate: fromDate,
-                toDate: toDate,
-                roomId: roomId
-            }
-            const response = await http.get('/schedule/week', { params })
-            runInAction('fetch schedule by week', () => {
-                if (response.status === 200) {
-                    this.scheduleByWeek = response.data
-                    this.year = response.data.year
-                    this.week = response.data.week
-                    this.roomId = response.data.roomId
-                    let day = moment(response.data.fromDate, 'DD/MM/YYYY')
-                    this.days = createDaysOfWeek(new Date(day))
-                }
-                this.loading = false
-            })
-        } catch (err) {
-            this.loading = false
+  @action getAllScheduleWithDetail = async (month, year) => {
+    this.loading = true
+    try {
+      let params = {
+        month: month,
+        year: year
+      }
+      const response = await http.get('/schedule/viewlist', { params })
+      runInAction('fetch schedule with detail', () => {
+        if (response.status === 200) {
+          this.scheduleWithDetails = response.data
         }
+        this.loading = false
+      })
+    } catch (err) {
+      this.loading = false
     }
+  }
 
-    @action getAllScheduleWithDetail = async (month, year) => {
-        this.loading = true
-        try {
-            let params = {
-                month: month,
-                year: year
-            }
-            const response = await http.get('/schedule/viewlist', { params })
-            runInAction('fetch schedule with detail', () => {
-                if (response.status === 200) {
-                    this.scheduleWithDetails = response.data
-                    this.loading = false
-                }
-            })
-        } catch (err) {
-            this.loading = false
+  @action getAllScheduleByUser = async (startValue, endValue) => {
+    this.loading = true
+    try {
+      let params = {
+        startValue: startValue.format('DD/MM/YYYY'),
+        endValue: endValue.format('DD/MM/YYYY')
+      }
+      const response = await http.get('/schedule/getbyuser', { params })
+      runInAction('fetch all by user', () => {
+        if (response.status === 200) {
+          this.scheduleByUsers = response.data
         }
+        this.loading = false
+      })
+    } catch (err) {
+      this.loading = false
     }
+  }
 
-    @action changeWeek = (week) => {
-        this.currentWeek = week
-    }
+  @action changeWeek = (week) => {
+    this.currentWeek = week
+  }
 
-    @action changeKeyword = (value) => {
-        this.keyword = value
-    }
+  @action changeKeyword = (value) => {
+    this.keyword = value
+  }
 
-    @computed get getScheduleByWeekJS() {
-        return toJS(this.scheduleByWeek)
-    }
+  @computed get getScheduleByWeekJS() {
+    return toJS(this.scheduleByWeek)
+  }
 
-    @computed get getScheduleWithDetailsJS() {
-        let data = toJS(this.scheduleWithDetails)
-        if (!!this.keyword) {
-            let keyword = this.keyword.toLowerCase()
-            data = data.filter(item => (item.groupPractice.teacher.lastName + ' ' + item.groupPractice.teacher.firstName).toLowerCase().indexOf(keyword) >= 0 ||
-                item.groupPractice.classId.toLowerCase().indexOf(keyword) >= 0 ||
-                item.description.toLowerCase().indexOf(keyword) >= 0 ||
-                item.groupPractice.subject.subjectName.toLowerCase().indexOf(keyword) >= 0
-            )
-        }
-        return data
+  @computed get getScheduleWithDetailsJS() {
+    let data = toJS(this.scheduleWithDetails)
+    if (!!this.keyword) {
+      let keyword = this.keyword.toLowerCase()
+      data = data.filter(item => (item.groupPractice.teacher.lastName + ' ' + item.groupPractice.teacher.firstName).toLowerCase().indexOf(keyword) >= 0 ||
+        item.groupPractice.classId.toLowerCase().indexOf(keyword) >= 0 ||
+        item.description.toLowerCase().indexOf(keyword) >= 0 ||
+        item.groupPractice.subject.subjectName.toLowerCase().indexOf(keyword) >= 0
+      )
     }
+    return data
+  }
 
-    @computed get daysOfWeek() {
-        return toJS(this.days)
-    }
+  @computed get getScheduleByUsersJS() {
+    return toJS(this.scheduleByUsers)
+  }
+
+  @computed get daysOfWeek() {
+    return toJS(this.days)
+  }
 
 }
 
 // eslint-disable-next-line no-extend-native
 Date.prototype.getWeek = function () {
-    var target = new Date(this.valueOf())
-    var dayNr = (this.getDay() + 6) % 7
-    target.setDate(target.getDate() - dayNr + 3)
-    var firstThursday = target.valueOf()
-    target.setMonth(0, 1)
-    if (target.getDay() !== 4) {
-        target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7)
-    }
-    return 1 + Math.ceil((firstThursday - target) / 604800000)
+  var target = new Date(this.valueOf())
+  var dayNr = (this.getDay() + 6) % 7
+  target.setDate(target.getDate() - dayNr + 3)
+  var firstThursday = target.valueOf()
+  target.setMonth(0, 1)
+  if (target.getDay() !== 4) {
+    target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7)
+  }
+  return 1 + Math.ceil((firstThursday - target) / 604800000)
 }
 
 function createDaysOfWeek(startDate) {
-    let data = []
-    for (let i = 1; i <= 7; i++) {
-        let dd = startDate.getDate()
-        let mm = startDate.getMonth() + 1
-        if (dd < 10) {
-            dd = `0${dd}`
-        }
-        if (mm < 10) {
-            mm = `0${mm}`
-        }
-        data.push(`${dd}/${mm}`)
-        startDate.setDate(startDate.getDate() + 1)
+  let data = []
+  for (let i = 1; i <= 7; i++) {
+    let dd = startDate.getDate()
+    let mm = startDate.getMonth() + 1
+    if (dd < 10) {
+      dd = `0${dd}`
     }
-    return data
+    if (mm < 10) {
+      mm = `0${mm}`
+    }
+    data.push(`${dd}/${mm}`)
+    startDate.setDate(startDate.getDate() + 1)
+  }
+  return data
 }
 
 export default new ScheduleStore()
